6dc1da12ce22eab095cd1b6e7666d655be21bdb1
koldan <dvk161105@gmail.com>
2026-02-20 18:25:58 +0500

    Add GeographicalImpact to home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@ import { HeroCombined } from "@/components/sections/hero-combined";
 import { VisualDiagram } from "@/components/sections/visual-diagram";
 import { ValueChainMap } from "@/components/sections/value-chain-map";
 
+import { GeographicalImpact } from "@/components/sections/geographical-impact";
+
 export default function Home() {
   return (
     <main className="bg-black min-h-screen">
       <HeroCombined />
       <VisualDiagram />
       <ValueChainMap />
+      <GeographicalImpact />
     </main>
   );
 }
